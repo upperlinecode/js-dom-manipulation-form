40fd0f2dbfcbf9e1ae707db95568316f254823ec
Ileini Melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 15:13:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,126 @@
+console.log("Script running!");
+
+// __________________________________________________________________________________________________________________________________ //
+// ***! STEP 1A OF CREATING A JS FORM - CODE QUERYSELECTORS FOR EACH COLUMN THAT CONTAINS USER INPUT ELEMENTS ON THE HTML PAGE !*** 
+
+// SYNTAX - coding a querySelector:
+// let variable_name = document.querySelector("#element-ID")
+
+// SYNTAX - coding a console.log statemtn to check querySelector:
+// console.log(variable_name)
+// __________________________________________________________________________________________________________________________________ //
+
+// ***! Lesson Code Along -  code below this line... !*** --> querySelectors
+
+
+
+
+// Code **querySelectors** for all the remaining input elements in the `Food Items` column below:
+
+
+
+// Code **console.log statements** below to check that you've selected ALL the desired elements in the `Food Items`column:
+
+
+
+
+// Code **querySelectors** for all the input elements in the `Household Items` column below:
+
+
+
+
+// Code **console.log statements** below to check that you've selected all the desired elements in the `Household Items` column:
+
+
+
+
+// Code **querySelectors** for all the input elements in the `Personal Items` column below:
+
+
+
+
+// Code **console.log statements** below to check that you've selected all the desired elements in the `Personal Items` column:
+
+
+
+
+// Code a **querySelector** for the "Enter" button below:
+
+
+// Code a **console.log statement** below to check that you've selected the button:
+
+
+// __________________________________________________________________________________________________________________________________ //
+// *** STEP 1B OF CREATING A JS FORM - CODE QUERYSELECTORS FOR EACH DIV WITHIN THE COLUMNS THAT WILL DISPLAY THE USER'S LIST ITEMS INPUT *** 
+// SYNTAX - coding a querySelector:
+// let variable_name = document.querySelector("#element-ID")
+
+// SYNTAX - coding a console.log statemtn to check querySelector:
+// console.log(variable_name)
+// __________________________________________________________________________________________________________________________________ //
+
+// Code a **querySelector** for each <div> element within the `Food Items` display column below:
+
+
+
+
+// Code **console.log statements** below to check that you've selected all the <div> elements in the `Food Items` display column:
+
+
+
+
+// Code a **querySelector** for each <div> element within the `Household Items` display column below:
+
+
+
+
+// Code **console.log statements** below to check that you've selected all the <div> elements in the `Household Items` display column:
+
+
+
+
+// Code a **querySelector** for each <div> element within the `Personal Items` display column below:
+
+
+
+
+// Code **console.log statements** below to check that you've selected all the <div> elements in the `Personal Items` display column:
+
+
+
+
+// __________________________________________________________________________________________________________________________________ //
+// ***! STEP 2A OF CREATING A JS FORM - CODE AN EVENT LISTENER FOR ALL THE FORM INPUT ELEMENTS (STEP 1A) ONCE THE "ENTER" BUTTON IS PRESSED !*** 
+
+// SYNTAX - coding a button event listener:
+//
+//     name_of_variable_for_button's_query_selector.addEventListener(`click`, (e) => {
+//        code instructions for what should happen if the button is pressed! 
+//      }) 
+// __________________________________________________________________________________________________________________________________ //
+// ***! STEP 2B OF CREATING A JS FORM - USE THE .INNERHTML PROPERTY TO ADD THE VALUE OF EACH FORM INPUT ELEMENT TO THE HTML PAGE !*** 
+
+// SYNTAX - coding a button event listener:
+//
+//     name_of_variable_for_button's_query_selector.addEventListener(`click`, (e) => {
+//        name_of_variable_for_display_queryselector += `${name_of_variable_for_input_element_querySelector.value}`
+//      }) 
+// __________________________________________________________________________________________________________________________________ //
+// ***! Lesson 2 Code Along -  code below this line... !*** --> Event Listeners
+
+
+
+
+// Code an **event listener** for the "Enter" button below:
+// Within the **event listener** use the **innerHTML** property to display the text the user input for each of the form input elements of the "Food Items:", "Household Items:", and "Personal Items:" lists. (Note: the user's list should be displayed under the titles, "Food:", "Household:" and "Personal:") below:
+
+
+
+
+
+
+
+
+
+
+
